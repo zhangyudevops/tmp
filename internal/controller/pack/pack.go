09a76b0aa4956a79aed88c7c23d8482d2bb21100
Aerys Targaryen <zhangyudevops@gmail.com>
2023-05-24 15:48:13 +0800

    pack full update pkg api

--- internal/controller/pack/pack.go
+++ internal/controller/pack/pack.go
@@ -55,9 +55,11 @@ func (c *cPack) PackUpdatePkg(ctx context.Context, req *apiv1.PackUpdatePkgReq) 
 	}
 
 	// @todo: 只是作为测试，如果解压了传统上面的images.ta.gz文件，需要把里层images目录下的文件移动到dstPath目录下,并删掉images目录
-	if err = gfile.Copy(dstPath+"/images", dstPath); err != nil {
-		_ = service.File().DeleteCurrentDir(ctx, dstPath+"/images")
-		return
+	if gfile.Exists(dstPath + "/images") {
+		if err = gfile.Move(dstPath+"/images", dstPath); err != nil {
+			//_ = service.File().DeleteCurrentDir(ctx, dstPath+"/images")
+			return
+		}
 	}
 
 	// request images list pull from harbor and save it to local

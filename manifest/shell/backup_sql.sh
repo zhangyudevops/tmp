6c49598eb5ed5f17ac3f84612a8318d0a368a1df
Napoleon Bonaparte <zhangyudevops@gmail.com>
2023-09-26 08:45:01 +0800

    back mysql script

--- manifest/shell/backup_sql.sh
+++ manifest/shell/backup_sql.sh
@@ -1,49 +1,34 @@
 #!/bin/bash
 #
-# incremental backup mysql data,sunday full backup, the other days of this week increment, and delete the backup files which are 21 days ago
-# use mysqldump to backup mysql data
+# mysql docker container name
+mysql="mysql-master"
 
-# define the backup path
-backup_dir=/data/backup/mysql
-# define the mysql data path
-mysql_data_dir=/data/mysql/data
-# define the mysql binlog path
-mysql_binlog_dir=/data/mysql/binlog
-# define the mysql user
-mysql_user=root
-# define the mysql password
-mysql_password=123456
-# define the mysql host
-mysql_host=localhost
-# define the mysql port
-mysql_port=3306
-# define the date format
-date_format=`date +%Y%m%d`
+# based today's date, create a folder in /backup/mysql
+# and backup all databases to that folder
+date=$(date +%Y-%m-%d)
+back_dir="/backup/mysql/$date"
 
-# define the backup file name
-backup_file_name=mysql-${date_format}.tar.gz
+# create backup folder, if not exists, create it
+if [ ! -d ${back_dir} ]; then
+  mkdir -p ${back_dir}
+fi
 
-# define the backup log file name
-backup_log_file_name=mysql_backup.log
+# 获取需要备份的数据库
+docker exec ${mysql} mysql -u root -paDmin1b3@ -e "SHOW DATABASES;" | grep -Ev "Database|mysql|information_schema|performance_schema|sys" > ${back_dir}/database_list.txt
 
-# define the backup log file path
-backup_log_file_path=${backup_dir}/${backup_log_file_name}
+# Backup MySQL databases
+while read -r database; do
+    docker exec ${mysql} /usr/bin/mysqldump -u root --password=aDmin1b3@ "$database" > "${back_dir}/$database.sql"
+done < ${back_dir}/database_list.txt
 
-# define the backup file path
-backup_file_path=${backup_dir}/${backup_file_name}
+# compress backup folder
+cd /backup/mysql
+tar -zcvf ${date}.tar.gz ${date}
 
-# define the mysql binlog file name
-mysql_binlog_file_name=mysql-bin.index
+# delete backup folder
+rm -rf ${back_dir}
 
-# define the mysql binlog file path
-mysql_binlog_file_path=${mysql_binlog_dir}/${mysql_binlog_file_name}
+# delete backup file 23 days ago
+find /backup/mysql/ -mtime +23 -name "*.tar.gz" -exec rm -f {} \;
 
-# define the mysql binlog backup file name
-mysql_binlog_backup_file_name=mysql-bin-${date_format}.tar.gz
-
-# define the mysql binlog backup file path
-mysql_binlog_backup_file_path=${backup_dir}/${mysql_binlog_backup_file_name}
-
-# define the mysql binlog backup log file name
-mysql_binlog_backup_log_file_name=mysql_binlog_backup.log
 
